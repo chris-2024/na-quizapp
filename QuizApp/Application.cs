f2c10f0342bb25a0ab000dd09aa35e72ad049114
chris <christofferjcm@gmail.com>
2023-11-17 08:41:58 +0100

    init

---
+++ QuizApp/Application.cs
@@ -0,0 +1,23 @@
+﻿namespace QuizApp;
+
+internal class Application
+{
+    public Application()
+    {
+        
+    }
+
+    public async Task Run()
+    {
+        do
+        {
+
+            await Console.Out.WriteLineAsync("App running...");
+
+            var b = Console.ReadKey().KeyChar;
+
+            if (b == 'e') break;
+
+        } while (true);
+    }
+}
---
+++ QuizApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace QuizApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+            .AddScoped<Application>()
+            // Menus
+
+            // Services
+
+            // Repositories
+
+            // Build
+            .BuildServiceProvider();
+
+            // Get application
+            var app = serviceProvider.GetRequiredService<Application>();
+
+            // Run application
+            await app.Run();
+        }
+    }
+}
