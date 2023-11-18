addbd640cba22739b03723c6aea52bf04ccdae6c
chris <christofferjcm@gmail.com>
2023-11-18 10:04:06 +0100

    Added lib models

---
+++ QuizApp.Lib/Models/Registrations/UserRegistration.cs
@@ -0,0 +1,10 @@
+﻿using QuizApp.Lib.Models.Entities;
+
+namespace QuizApp.Lib.Models.Registrations;
+
+public class UserRegistration
+{
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public UserRole UserRole { get; set; }
+}
---
+++ QuizApp.Lib/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using QuizApp.Lib.Models.Entities;
+
+namespace QuizApp.Lib.Models;
+
+public class User
+{
+    public string Username { get; set; } = null!;
+    public UserRole UserRole { get; set; }
+
+    public static implicit operator User(UserEntity user)
+    {
+        return new User()
+        {
+            Username = user.Username,
+            UserRole = (UserRole)user.UserRoleID
+        };
+    }
+}
+
+public enum UserRole
+{
+    None,
+    Registered,
+    Guest
+}
